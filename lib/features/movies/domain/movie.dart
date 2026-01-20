5a6206aa2b69eff27534fbe11000ec095d839708
nedlab <nedlab@yahoo.com>
2026-01-20 11:57:26 +0200

    fixed Movie

--- lib/features/movies/domain/movie.dart
+++ lib/features/movies/domain/movie.dart
@@ -4,26 +4,19 @@ part 'movie.g.dart';
 
 @JsonSerializable()
 class Movie {
-
   final int id;
   final String title;
-  final String overview;
   @JsonKey(name: 'poster_path')
   final String? posterPath;
 
   @JsonKey(name: 'vote_average')
   final double? rating;
 
-  @JsonKey(name: 'release_date')
-  final DateTime? releaseDate;
-
   Movie({
     required this.id,
     required this.title,
-    required this.overview,
     this.posterPath,
     this.rating,
-    this.releaseDate,
   });
 
   factory Movie.fromJson(Map<String, dynamic> json) => _$MovieFromJson(json);
