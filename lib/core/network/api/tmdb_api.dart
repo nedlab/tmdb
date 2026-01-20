3ba9c8341a94b68220f03a2a9085d6206b5c63bf
nedlab <s.nedelko@betinvest.com>
2026-01-20 13:25:22 +0200

    + interface TmdbApi

---
+++ lib/core/network/api/tmdb_api.dart
@@ -0,0 +1,10 @@
+
+import 'package:tmdb/features/movie_details/domain/movie_details.dart';
+import 'package:tmdb/features/movies/data/models/movie_response.dart';
+import 'package:tmdb/utils/result.dart';
+
+abstract class TmdbApi {
+  Future<Result<MovieResponse>> getTopRatedMovies({int page = 1});
+  Future<Result<MovieDetails>> getMovieDetails(int movieId);
+  Future<Result<MovieResponse>> searchMovies(String query, {int page = 1});
+}
