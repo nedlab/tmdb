2a67a59cc120c0a10fe8fac7e6598cf8e7f223a1
nedlab <nedlab@yahoo.com>
2026-01-22 14:11:23 +0200

    + top rated movies provider

---
+++ lib/features/movies/presentation/providers/top_movies_provider.g.dart
@@ -0,0 +1,58 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'top_movies_provider.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+// GENERATED CODE - DO NOT MODIFY BY HAND
+// ignore_for_file: type=lint, type=warning
+
+@ProviderFor(TopRatedMoviesNotifier)
+final topRatedMoviesProvider = TopRatedMoviesNotifierProvider._();
+
+final class TopRatedMoviesNotifierProvider
+    extends
+        $AsyncNotifierProvider<TopRatedMoviesNotifier, TopRatedMoviesState> {
+  TopRatedMoviesNotifierProvider._()
+    : super(
+        from: null,
+        argument: null,
+        retry: null,
+        name: r'topRatedMoviesProvider',
+        isAutoDispose: true,
+        dependencies: null,
+        $allTransitiveDependencies: null,
+      );
+
+  @override
+  String debugGetCreateSourceHash() => _$topRatedMoviesNotifierHash();
+
+  @$internal
+  @override
+  TopRatedMoviesNotifier create() => TopRatedMoviesNotifier();
+}
+
+String _$topRatedMoviesNotifierHash() =>
+    r'715579f8b8f5358727498debaf0cbbf5557b6920';
+
+abstract class _$TopRatedMoviesNotifier
+    extends $AsyncNotifier<TopRatedMoviesState> {
+  FutureOr<TopRatedMoviesState> build();
+  @$mustCallSuper
+  @override
+  void runBuild() {
+    final ref =
+        this.ref as $Ref<AsyncValue<TopRatedMoviesState>, TopRatedMoviesState>;
+    final element =
+        ref.element
+            as $ClassProviderElement<
+              AnyNotifier<AsyncValue<TopRatedMoviesState>, TopRatedMoviesState>,
+              AsyncValue<TopRatedMoviesState>,
+              Object?,
+              Object?
+            >;
+    element.handleCreate(ref, build);
+  }
+}
