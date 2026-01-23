c380e2345651c4e32969adfee4f877e19a41b4dc
nedlab <s.nedelko@betinvest.com>
2026-01-23 12:18:10 +0200

    + class MoviePoster

--- lib/features/movies/presentation/widgets/movie_card.dart
+++ lib/features/movies/presentation/widgets/movie_card.dart
@@ -1,15 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:tmdb/features/movies/domain/movie.dart';
+import 'package:tmdb/features/movies/presentation/widgets/movie_poster.dart';
 
 class MovieCard extends StatelessWidget {
   final Movie movie;
   final VoidCallback? onTap;
 
-  const MovieCard({
-    super.key,
-    required this.movie,
-    this.onTap,
-  });
+  const MovieCard({super.key, required this.movie, this.onTap});
 
   @override
   Widget build(BuildContext context) {
@@ -27,7 +24,7 @@ class MovieCard extends StatelessWidget {
             Expanded(
               child: ClipRRect(
                 borderRadius: BorderRadius.circular(15),
-                child: _Poster(posterUrl: posterUrl),
+                child: MoviePoster(posterUrl: posterUrl),
               ),
             ),
             const SizedBox(height: 8),
@@ -45,39 +42,6 @@ class MovieCard extends StatelessWidget {
           ],
         ),
       ),
-    );
-  }
-}
-
-class _Poster extends StatelessWidget {
-  final String? posterUrl;
-
-  const _Poster({required this.posterUrl});
-
-  @override
-  Widget build(BuildContext context) {
-    if (posterUrl == null || posterUrl!.isEmpty) {
-      return const ColoredBox(
-        color: Color(0xFF1E1E1E),
-        child: Center(
-          child: Icon(Icons.image_not_supported_outlined, size: 40),
-        ),
-      );
-    }
-
-    return Image.network(
-      posterUrl!,
-      fit: BoxFit.cover,
-      width: double.infinity,
-      height: double.infinity,
-      errorBuilder: (context, error, stackTrace) {
-        return const ColoredBox(
-          color: Color(0xFF1E1E1E),
-          child: Center(
-            child: Icon(Icons.broken_image_outlined, size: 40),
-          ),
-        );
-      },
     );
   }
 }
