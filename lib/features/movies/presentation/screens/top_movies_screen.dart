a092fe3dd4fc2d316dd0bd2d6ca1ea0e3d7734a8
nedlab <s.nedelko@betinvest.com>
2026-01-26 19:05:54 +0200

    movies grid moved to class MoviesView

--- lib/features/movies/presentation/screens/top_movies_screen.dart
+++ lib/features/movies/presentation/screens/top_movies_screen.dart
@@ -2,9 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
 import 'package:tmdb/app/router/routes.dart';
-import 'package:tmdb/features/favorites/favorites_provider.dart';
 import 'package:tmdb/features/movies/presentation/providers/top_movies_provider.dart';
-import 'package:tmdb/features/movies/presentation/widgets/movie_card.dart';
+import 'package:tmdb/features/movies/presentation/widgets/movies_view.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:tmdb/core/widgets/loader.dart' as app_loader;
 
@@ -34,50 +33,11 @@ class TopMoviesScreen extends ConsumerWidget {
         actionsPadding: EdgeInsets.only(right: 16)
       ),
       body: state.when(
-        data: (value) => LayoutBuilder(
-          builder: (context, constraints) {
-            const padding = 16.0;
-            const crossAxisCount = 2;
-            const spacing = 16.0;
-            const posterAspectRatio = 163 / 269;
-
-            final gridWidth = constraints.maxWidth - (padding * 2);
-            final tileWidth =
-                (gridWidth - ((crossAxisCount - 1) * spacing)) / crossAxisCount;
-
-            final posterHeight = tileWidth / posterAspectRatio;
-            const textAreaHeight = 60.0;
-            final tileHeight = posterHeight + textAreaHeight;
-
-            return GridView.builder(
-              padding: const EdgeInsets.all(padding),
-              gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                crossAxisCount: crossAxisCount,
-                mainAxisExtent: tileHeight,
-                crossAxisSpacing: spacing,
-                mainAxisSpacing: spacing,
-              ),
-              itemCount: value.movies.length,
-              itemBuilder: (context, index) {
-                final movie = value.movies[index];
-
-                return MovieCard(
-                  movie: movie,
-                  isFavorite: ref.watch(isFavoriteProvider(movie.id)),
-                  onTap: () {
-                    final path = AppRoutes.movieDetails.replaceFirst(
-                      ':id',
-                      movie.id.toString(),
-                    );
-
-                    final title = Uri.encodeComponent(movie.title);
-                    context.push('$path?title=$title');
-                  },
-                );
-              },
-            );
-          },
-        ),
+        data:
+            (value) => MoviesView(
+              movies: value.movies,
+              padding: const EdgeInsets.all(16.0),
+            ),
         error: (error, _) => Text('Error $error'),
         loading: () => const Center(child: app_loader.SvgLoader()),
       ),
---
+++ lib/features/movies/presentation/widgets/movies_view.dart
@@ -0,0 +1,66 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:go_router/go_router.dart';
+import 'package:tmdb/app/router/routes.dart';
+import 'package:tmdb/features/favorites/favorites_provider.dart';
+import 'package:tmdb/features/movies/domain/movie.dart';
+import 'package:tmdb/features/movies/presentation/widgets/movie_card.dart';
+
+class MoviesView extends ConsumerWidget {
+  final List<Movie> movies;
+  final EdgeInsetsGeometry padding;
+
+  const MoviesView({
+    super.key,
+    required this.movies,
+    this.padding = EdgeInsets.zero,
+  });
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    return LayoutBuilder(
+      builder: (context, constraints) {
+        const crossAxisCount = 2;
+        const spacing = 16.0;
+        const posterAspectRatio = 163 / 269;
+
+        // Calculate expected padding width to subtract from total width for column width calculation
+        final double horizontalPadding = padding.horizontal;
+
+        final gridWidth = constraints.maxWidth - horizontalPadding;
+        final tileWidth =
+            (gridWidth - ((crossAxisCount - 1) * spacing)) / crossAxisCount;
+
+        final posterHeight = tileWidth / posterAspectRatio;
+        const textAreaHeight = 60.0;
+        final tileHeight = posterHeight + textAreaHeight;
+
+        return GridView.builder(
+          padding: padding,
+          gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+            crossAxisCount: crossAxisCount,
+            mainAxisExtent: tileHeight,
+            crossAxisSpacing: spacing,
+            mainAxisSpacing: spacing,
+          ),
+          itemCount: movies.length,
+          itemBuilder: (context, index) {
+            final movie = movies[index];
+            return MovieCard(
+              movie: movie,
+              isFavorite: ref.watch(isFavoriteProvider(movie.id)),
+              onTap: () {
+                final path = AppRoutes.movieDetails.replaceFirst(
+                  ':id',
+                  movie.id.toString(),
+                );
+                final title = Uri.encodeComponent(movie.title);
+                context.push('$path?title=$title');
+              },
+            );
+          },
+        );
+      },
+    );
+  }
+}
