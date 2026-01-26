fc5f746085799a69ab5a2e7784273d5a18c47069
nedlab <nedlab@yahoo.com>
2026-01-26 17:05:45 +0200

    SearchMovieScreen: + input

--- lib/features/search/presentation/screens/search_movie_screen.dart
+++ lib/features/search/presentation/screens/search_movie_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 class SearchMovieScreen extends ConsumerStatefulWidget {
   const SearchMovieScreen({super.key});
@@ -9,8 +10,58 @@ class SearchMovieScreen extends ConsumerStatefulWidget {
 }
 
 class _SearchMovieScreenState extends ConsumerState<SearchMovieScreen> {
+  late final TextEditingController _controller;
+
+  @override
+  void initState() {
+    _controller = TextEditingController();
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(appBar: AppBar(title: Text('Search'),),);
+    return Scaffold(
+      appBar: AppBar(title: Text('Search')),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          children: [
+            TextField(
+              controller: _controller,
+              decoration: InputDecoration(
+                filled: true,
+                fillColor: Theme.of(
+                  context,
+                ).colorScheme.surfaceContainerHighest, // Світло-сірий фон
+                hintText: 'Search',
+                prefixIcon: Padding(
+                  padding: const EdgeInsets.all(12.0),
+                  child: SvgPicture.asset(
+                    'assets/images/Search.svg',
+                    width: 20,
+                    height: 20,
+                    colorFilter: ColorFilter.mode(
+                      Theme.of(context).colorScheme.onSurfaceVariant,
+                      BlendMode.srcIn,
+                    ),
+                  ),
+                ),
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(12),
+                  borderSide: BorderSide.none,
+                ),
+              ),
+            ),
+            Expanded(child: SizedBox.expand())
+          ],
+        ),
+      ),
+    );
   }
 }
