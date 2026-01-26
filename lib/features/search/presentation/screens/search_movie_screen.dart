42e9951bfb308f51abbc9d171d64cae7bca37a8f
nedlab <nedlab@yahoo.com>
2026-01-26 19:05:54 +0200

    movies grid moved to class MoviesView

--- lib/features/search/presentation/screens/search_movie_screen.dart
+++ lib/features/search/presentation/screens/search_movie_screen.dart
@@ -1,10 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_svg/flutter_svg.dart';
-import 'package:go_router/go_router.dart';
-import 'package:tmdb/app/router/routes.dart';
-import 'package:tmdb/features/favorites/favorites_provider.dart';
-import 'package:tmdb/features/movies/presentation/widgets/movie_card.dart';
+import 'package:tmdb/features/movies/presentation/widgets/movies_view.dart';
 import 'package:tmdb/features/search/presentation/providers/movie_search_provider.dart';
 
 class SearchMovieScreen extends ConsumerStatefulWidget {
@@ -97,49 +94,7 @@ class _SearchMovieScreenState extends ConsumerState<SearchMovieScreen> {
                       child: SvgPicture.asset('assets/images/NotFound.svg'),
                     );
                   }
-                  return LayoutBuilder(
-                    builder: (context, constraints) {
-                      const padding = 0.0;
-                      const crossAxisCount = 2;
-                      const spacing = 16.0;
-                      const posterAspectRatio = 163 / 269;
-
-                      final gridWidth = constraints.maxWidth - (padding * 2);
-                      final tileWidth =
-                          (gridWidth - ((crossAxisCount - 1) * spacing)) /
-                          crossAxisCount;
-
-                      final posterHeight = tileWidth / posterAspectRatio;
-                      const textAreaHeight = 60.0;
-                      final tileHeight = posterHeight + textAreaHeight;
-
-                      return GridView.builder(
-                        padding: const EdgeInsets.all(padding),
-                        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                          crossAxisCount: crossAxisCount,
-                          mainAxisExtent: tileHeight,
-                          crossAxisSpacing: spacing,
-                          mainAxisSpacing: spacing,
-                        ),
-                        itemCount: movies.length,
-                        itemBuilder: (context, index) {
-                          final movie = movies[index];
-                          return MovieCard(
-                            movie: movie,
-                            isFavorite: ref.watch(isFavoriteProvider(movie.id)),
-                            onTap: () {
-                              final path = AppRoutes.movieDetails.replaceFirst(
-                                ':id',
-                                movie.id.toString(),
-                              );
-                              final title = Uri.encodeComponent(movie.title);
-                              context.push('$path?title=$title');
-                            },
-                          );
-                        },
-                      );
-                    },
-                  );
+                  return MoviesView(movies: movies);
                 },
                 orElse: () => SizedBox.shrink(),
               ),
