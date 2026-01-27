ac2f9fb39684df3aec54254224ddb132548adf24
nedlab <s.nedelko@betinvest.com>
2026-01-27 14:49:25 +0200

    + switch theme light/dart with default style

--- lib/app/app.dart
+++ lib/app/app.dart
@@ -1,13 +1,24 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:tmdb/app/router/app_router.dart';
+import 'package:tmdb/app/theme/app_theme.dart';
+import 'package:tmdb/app/theme/theme_provider.dart';
 
-class MyApp extends StatelessWidget {
+class MyApp extends ConsumerWidget {
   const MyApp({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    final themeMode = ref.watch(appThemeProvider).maybeWhen(
+      data: (value) => value,
+      orElse: () => ThemeMode.system,
+    );
+
     return MaterialApp.router(
       debugShowCheckedModeBanner: false,
+      theme: AppTheme.lightTheme,
+      darkTheme: AppTheme.darkTheme,
+      themeMode: themeMode,
       routerConfig: appRouter,
     );
   }
