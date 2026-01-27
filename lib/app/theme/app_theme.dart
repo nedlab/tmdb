af2259c320f7c57ab1d1a423e9cc2c01de92469e
nedlab <nedlab@yahoo.com>
2026-01-27 14:49:25 +0200

    + switch theme light/dart with default style

---
+++ lib/app/theme/app_theme.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class AppTheme {
+  static ThemeData get lightTheme => ThemeData(
+    brightness: Brightness.light
+  );
+
+  static ThemeData get darkTheme => ThemeData(
+    brightness: Brightness.dark
+  );
+
+}
